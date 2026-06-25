d4404d3cce6b3b41992f50556c28ddb43daf3ae2
Timo_01 <archeagelol197@gmail.com>
2026-06-26 00:18:11 +0200

    Add Character and WorldGrid

---
+++ Testing/Assets/CharacterController2d.cs
@@ -0,0 +1,55 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    private static readonly int HorizontalHash = Animator.StringToHash("Horizontal");
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float speed = 2f;
+    Vector2 motionVector;
+    Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+
+        if (animator.GetFloat("Horizontal") == 0 && animator.GetFloat("Vertical") == 0)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        else
+        {
+            animator.SetBool("isMoving", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        rigidbody2d.linearVelocity = motionVector * speed;
+
+
+
+    }
+}
