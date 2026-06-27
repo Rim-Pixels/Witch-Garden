ba43bec34138c4dd6dd7a19702facbd7e34507b8
QuinKing <bquinieboy2000@gmail.com>
2026-06-27 16:05:44 +0200

    grid options
    added more possible options for the grid, using 1/2/3/4/5/6 you can change the type (not yet fully working)

--- Testing/Assets/Scripts/DualGridTilemap.cs
+++ Testing/Assets/Scripts/DualGridTilemap.cs
@@ -6,6 +6,8 @@ using UnityEngine.Tilemaps;
 using static TileType;
 
 public class DualGridTilemap : MonoBehaviour {
+
+
     protected static Vector3Int[] NEIGHBOURS = new Vector3Int[] {
         new Vector3Int(0, 0, 0),
         new Vector3Int(1, 0, 0),
@@ -13,7 +15,7 @@ public class DualGridTilemap : MonoBehaviour {
         new Vector3Int(1, 1, 0)
     };
 
-    protected static Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> neighbourTupleToTile;
+    protected Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> neighbourTupleToTile;
 
     // Provide references to each tilemap in the inspector
     public Tilemap placeholderTilemap;
@@ -22,33 +24,90 @@ public class DualGridTilemap : MonoBehaviour {
     // Provide the dirt and grass placeholder tiles in the inspector
     public Tile grassPlaceholderTile;
     public Tile dirtPlaceholderTile;
+    public Tile stonePlaceholderTile;
+    public Tile waterPlaceholderTile;
 
     // Provide the 16 tiles in the inspector
     public Tile[] tiles;
 
     void Start() {
+        neighbourTupleToTile = new();
+        AddRules(Grass, Dirt, 0);      // tiles[0–15]
+        AddRules(Stone, Grass, 16);    // tiles[16–31]
+        AddRules(Stone, Dirt, 32);     // tiles[32–47]
+        AddRules(Stone, Water, 48);    // tiles[48–63]
+        AddRules(Water, Grass, 64);    // tiles[64–79]
+        AddRules(Water, Dirt, 80);     // tiles[80–95]
+        FillAreaWithGrass(20);
+        RefreshDisplayTilemap();
+    }
+
+    private void AddRules(TileType A, TileType B, int offset){
         // This dictionary stores the "rules", each 4-neighbour configuration corresponds to a tile
         // |_1_|_2_|
         // |_3_|_4_|
-        neighbourTupleToTile = new() {
-            {new (Grass, Grass, Grass, Grass), tiles[6]},
-            {new (Dirt, Dirt, Dirt, Grass), tiles[13]}, // OUTER_BOTTOM_RIGHT
-            {new (Dirt, Dirt, Grass, Dirt), tiles[0]}, // OUTER_BOTTOM_LEFT
-            {new (Dirt, Grass, Dirt, Dirt), tiles[8]}, // OUTER_TOP_RIGHT
-            {new (Grass, Dirt, Dirt, Dirt), tiles[15]}, // OUTER_TOP_LEFT
-            {new (Dirt, Grass, Dirt, Grass), tiles[1]}, // EDGE_RIGHT
-            {new (Grass, Dirt, Grass, Dirt), tiles[11]}, // EDGE_LEFT
-            {new (Dirt, Dirt, Grass, Grass), tiles[3]}, // EDGE_BOTTOM
-            {new (Grass, Grass, Dirt, Dirt), tiles[9]}, // EDGE_TOP
-            {new (Dirt, Grass, Grass, Grass), tiles[5]}, // INNER_BOTTOM_RIGHT
-            {new (Grass, Dirt, Grass, Grass), tiles[2]}, // INNER_BOTTOM_LEFT
-            {new (Grass, Grass, Dirt, Grass), tiles[10]}, // INNER_TOP_RIGHT
-            {new (Grass, Grass, Grass, Dirt), tiles[7]}, // INNER_TOP_LEFT
-            {new (Dirt, Grass, Grass, Dirt), tiles[14]}, // DUAL_UP_RIGHT
-            {new (Grass, Dirt, Dirt, Grass), tiles[4]}, // DUAL_DOWN_RIGHT
-            {new (Dirt, Dirt, Dirt, Dirt), tiles[12]},
-        };
-        RefreshDisplayTilemap();
+        neighbourTupleToTile.Add(new(A, A, A, A), tiles[offset + 6]);
+        neighbourTupleToTile.Add(new(B, B, B, A), tiles[offset + 13]);
+        neighbourTupleToTile.Add(new(B, B, A, B), tiles[offset + 0]);
+        neighbourTupleToTile.Add(new(B, A, B, B), tiles[offset + 8]);
+        neighbourTupleToTile.Add(new(A, B, B, B), tiles[offset + 15]);
+        neighbourTupleToTile.Add(new(B, A, B, A), tiles[offset + 1]);
+        neighbourTupleToTile.Add(new(A, B, A, B), tiles[offset + 11]);
+        neighbourTupleToTile.Add(new(B, B, A, A), tiles[offset + 3]);
+        neighbourTupleToTile.Add(new(A, A, B, B), tiles[offset + 9]);
+        neighbourTupleToTile.Add(new(B, A, A, A), tiles[offset + 5]);
+        neighbourTupleToTile.Add(new(A, B, A, A), tiles[offset + 2]);
+        neighbourTupleToTile.Add(new(A, A, B, A), tiles[offset + 10]);
+        neighbourTupleToTile.Add(new(A, A, A, B), tiles[offset + 7]);
+        neighbourTupleToTile.Add(new(B, A, A, B), tiles[offset + 14]);
+        neighbourTupleToTile.Add(new(A, B, B, A), tiles[offset + 4]);
+        neighbourTupleToTile.Add(new(B, B, B, B), tiles[offset + 12]);
+        FillAreaWithGrass(20);
+        RefreshDisplayTilemap();  
+    }
+
+    private (TileType A, TileType B, int offset) GetPair(TileType topLeft, TileType topRight, TileType botLeft, TileType botRight)
+    {
+        // Collect all tile types in the block
+        TileType[] arr = {topLeft, topRight, botLeft, botRight};
+
+        // Find the first non-None tile
+        TileType A = TileType.None;
+        for (int i = 0; i < 4; i++)
+        {
+            if (arr[i] != None)
+            {
+                A = arr[i];
+                break;
+            }
+        }
+
+        if (A == None)
+            return (None, None, -1);
+
+        // Find a second different tile
+        TileType B = A;
+        for (int i = 0; i < 4; i++)
+        {
+            if (arr[i] != None && arr[i] != A)
+            {
+                B = arr[i];
+                break;
+            }
+        }
+
+        // If all tiles are the same
+        if (A == B)
+            return (A, A, 0);
+
+        // Determine offset
+        if ((A == Grass & B == Dirt) | (A == Dirt & B == Grass)) return (Grass, Dirt, 0);
+        if ((A == Stone & B == Grass) | (A == Grass & B == Stone)) return (Stone, Grass, 16);
+        if ((A == Stone & B == Dirt) | (A == Dirt & B == Stone)) return (Stone, Dirt, 32);
+        if ((A == Stone & B == Water) | (A == Water & B == Stone)) return (Stone, Water, 48);
+        if ((A == Water & B == Grass) | (A == Grass & B == Water)) return (Water, Grass, 64);
+        if ((A == Water & B == Dirt) | (A == Dirt & B == Water)) return (Water, Dirt, 80);
+        return (None, None, -1);
     }
 
     public void SetCell(Vector3Int coords, Tile tile) {
@@ -57,10 +116,12 @@ public class DualGridTilemap : MonoBehaviour {
     }
 
     private TileType getPlaceholderTileTypeAt(Vector3Int coords) {
-        if (placeholderTilemap.GetTile(coords) == grassPlaceholderTile)
-            return Grass;
-        else
-            return Dirt;
+        TileBase t = placeholderTilemap.GetTile(coords);
+        if (t == grassPlaceholderTile) return Grass;
+        if (t == dirtPlaceholderTile) return Dirt;
+        if (t == stonePlaceholderTile) return Stone;
+        if (t == waterPlaceholderTile) return Water;
+        return None;
     }
 
     protected Tile calculateDisplayTile(Vector3Int coords) {
@@ -70,15 +131,41 @@ public class DualGridTilemap : MonoBehaviour {
         TileType botRight = getPlaceholderTileTypeAt(coords - NEIGHBOURS[2]);
         TileType botLeft = getPlaceholderTileTypeAt(coords - NEIGHBOURS[3]);
 
+        // Determine which tile pair we are in
+        var (A, B, offset) = GetPair(topLeft, topRight, botLeft, botRight);
+
+        if (offset < 0)
+            return null;
+
         Tuple<TileType, TileType, TileType, TileType> neighbourTuple = new(topLeft, topRight, botLeft, botRight);
 
-        return neighbourTupleToTile[neighbourTuple];
+        if (neighbourTupleToTile.TryGetValue(neighbourTuple, out Tile tile))
+            return tile;
+
+        return null;
     }
 
     protected void setDisplayTile(Vector3Int pos) {
         for (int i = 0; i < NEIGHBOURS.Length; i++) {
             Vector3Int newPos = pos + NEIGHBOURS[i];
             displayTilemap.SetTile(newPos, calculateDisplayTile(newPos));
+        }
+    }
+
+    private void FillAreaWithGrass(int radius)
+    {
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                Vector3Int pos = new Vector3Int(x, y, 0);
+
+                // Only fill empty tiles
+                if (placeholderTilemap.GetTile(pos) == null)
+                {
+                    placeholderTilemap.SetTile(pos, grassPlaceholderTile);
+                }
+            }
         }
     }
 
@@ -95,5 +182,7 @@ public class DualGridTilemap : MonoBehaviour {
 public enum TileType {
     None,
     Grass,
-    Dirt
+    Dirt,
+    Stone,
+    Water
 }
